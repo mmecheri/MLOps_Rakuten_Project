bfdf0bdbeeef581d72097182da5130cd683e8b2f
Mourad MECHERI <88212289+mmecheri@users.noreply.github.com>
2022-09-16 14:04:10 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -88,8 +88,8 @@ async def authenticate(request: Request, call_next):
     response = await call_next(request)
     return response
 
- #                           Default Routes                     
- #-----------------------------------------------------------------------------------------------
+#-------------------------Default Routes--------------------------------------------------------                     
+#-----------------------------------------------------------------------------------------------
 @app.get("/",tags=['Default'])
 async def root():
     """  
@@ -124,7 +124,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return {"access_token": access_token, "token_type": "bearer"}
 #-----------------------------------------------------------------------------------------------
 
-#                           Include Admin , Users and Models predictions Routes                     
+
+#-------------------------Include Admin , Users and Models predictions Routes-------------------                     
 #-----------------------------------------------------------------------------------------------
 app.include_router(adminRouter,tags=['Admin']) # Admin Routes
 app.include_router(usersRouter,tags=['Users']) # Users Routes
