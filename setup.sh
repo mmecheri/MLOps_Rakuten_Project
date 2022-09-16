fd272c6fa69f9ea294f5c0867f1245c647908c50
Ubuntu <ubuntu@ip-172-31-25-123.eu-west-1.compute.internal>
2022-09-16 17:11:03 +0000

    setup Chmod update

--- setup.sh
+++
@@ -1,7 +0,0 @@
-docker image build authentication_image/ -t authentication_image:latest
-docker image build authorization_image/ -t authorization_image:latest
-docker image build prediction_image/ -t prediction_image:latest
-
-docker-compose up --force-recreate -V
-
-
