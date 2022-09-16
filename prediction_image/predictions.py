4b7d1b518db196a04d8f929b0973195c90d47c22
Mourad MECHERI <88212289+mmecheri@users.noreply.github.com>
2022-09-16 13:17:51 +0200

    Update predictions.py

--- prediction_image/predictions.py
+++ prediction_image/predictions.py
@@ -10,10 +10,10 @@ date_test = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
 log_fname =  '/home/data/api_tests.log' 
 
 
-# définition de l'adresse de l'API
+# API IP
 api_address = 'fastApi_rakuten'
 
-# port de l'API
+# API port
 api_port = 8000
 
 
@@ -51,10 +51,9 @@ test_title =  '''
 test_title =  test_title.format(date_test = date_test)
 print(test_title)
 
-# requête #1 START =========================================================================================
+# Request #1 START =========================================================================================
 designation_text_1 = "Figurine Minnie Robe Jaune Et Chaussure Rose"
 description_text_1 = "quelques petit éclats de peinture sur gant nez et oreille blanc"
-
 real_class_1 = 1280
 real_label_1 = 'toys for children'
 
@@ -94,10 +93,10 @@ actual Precision  = {precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_1.status_code
 
-# score de la requête
+# Request response
 results_1 = response_1.json()
 #results_1
 
@@ -105,12 +104,7 @@ predicted_class = results_1["predicted_class"]
 predicted_label = results_1["predicted_label"]
 precision = results_1["precision"]
 
-#print('status_code : ' , status_code)
-#print('predicted_class : ' , predicted_class)
-#print('predicted_label : ' , predicted_label)
-#print('precision : ' , precision)
-
-# affichage des résultats code response
+# Code response results
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_1:    
@@ -119,10 +113,8 @@ if status_code == 200 :
         prediction_status = 'FAILURE'
     
 elif status_code != 200:
-    #test_status = 'FAILURE'
     score_status = 'FAILURE'
 
-    
 output_1 =  output_1.format(designation_text_1=designation_text_1,
                             description_text_1=description_text_1,
                             real_class_1=real_class_1,
@@ -134,11 +126,9 @@ output_1 =  output_1.format(designation_text_1=designation_text_1,
                             precision=precision,
                            prediction_status=prediction_status)
 
-
 print(output_1)
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
         print("LOGs saved on",  log_fname)
@@ -146,12 +136,11 @@ if os.environ.get('LOG') == '1':
         file.write(output_1)
 else : 
      print("An error occurred when tring to save LOGs!")
-# requête #1 END =========================================================================================
+#Request #1 END =========================================================================================
 
-# requête #2 START =========================================================================================
+#Request #2 START =========================================================================================
 designation_text_2 = "Revue Fémina 2004 Couverture Sharon Stone"
 description_text_2 = None
-
 real_class_2 = 2403
 real_label_2 = 'children books and magazines'
 
@@ -192,10 +181,10 @@ actual Precision  = {precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_2.status_code
 
-# score de la requête
+# Request response
 results_2 = response_2.json()
 #results_2
 
@@ -203,12 +192,7 @@ predicted_class = results_2["predicted_class"]
 predicted_label = results_2["predicted_label"]
 precision = results_2["precision"]
 
-#print('status_code : ' , status_code)
-#print('predicted_class : ' , predicted_class)
-#print('predicted_label : ' , predicted_label)
-#print('precision : ' , precision)
-
-# affichage des résultats code response
+# Code response result
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_2:    
@@ -217,7 +201,6 @@ if status_code == 200 :
         prediction_status = 'FAILURE'
     
 elif status_code != 200:
-    #test_status = 'FAILURE'
     score_status = 'FAILURE'
 
     
@@ -235,24 +218,21 @@ output_2 =  output_2.format(designation_text_2=designation_text_2,
 
 print(output_2)
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
-        print("LOGs saved on",  log_fname)
-        #file.write(test_title)
+        print("LOGs saved on",  log_fname)      
         file.write(output_2)
 else : 
      print("An error occurred when tring to save LOGs!")
-# requête #2 END =========================================================================================
+#Request #2 END =========================================================================================
 
 headers = {}
  
 # Add the token to subsequent requests
 headers['Authorization'] = 'Bearer ' + token
 
-# requête #3 START =========================================================================================
-
+#Request #3 START ========================================================================================
 image_name_1 = './image_samples/image_1164498572_product_2786150067.jpg'
 real_class_3 = 2583
 real_label_3 = 'piscine spa'
@@ -298,10 +278,10 @@ actual Precision  = {precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_3.status_code
 
-# score de la requête
+# Request response
 results_3 = response_3.json()
 #results_3
 
@@ -309,7 +289,7 @@ predicted_class = results_3["predicted_class"]
 predicted_label = results_3["predicted_label"]
 precision = results_3["precision"]
 
-# affichage des résultats code response
+# Code response result
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_3:    
@@ -317,8 +297,7 @@ if status_code == 200 :
     else: 
         prediction_status = 'FAILURE'
     
-elif status_code != 200:
-    #test_status = 'FAILURE'
+elif status_code != 200:   
     score_status = 'FAILURE'
 
 output_3 =  output_3.format(image_name_1=image_name_1,                           
@@ -334,18 +313,16 @@ output_3 =  output_3.format(image_name_1=image_name_1,
 
 print(output_3)
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
-        print("LOGs saved on",  log_fname)
-        #file.write(test_title)
+        print("LOGs saved on",  log_fname)       
         file.write(output_3)
 else : 
      print("An error occurred when tring to save LOGs!")
-# requête #3 END =========================================================================================
+#Request #3 END ===========================================================================================
 
-# requête #4 START =========================================================================================
+#Request #4 START =========================================================================================
 image_name_2 = './image_samples/image_862500067_product_102168018.jpg'
 real_class_4 = 1160
 real_label_4 = 'playing cards'
@@ -390,10 +367,10 @@ actual Precision  = {precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_4.status_code
 
-# score de la requête
+# Request result
 results_4 = response_4.json()
 #results_4
 
@@ -401,7 +378,7 @@ predicted_class = results_4["predicted_class"]
 predicted_label = results_4["predicted_label"]
 precision = results_4["precision"]
 
-# affichage des résultats code response
+# Code response result
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_4:    
@@ -410,7 +387,6 @@ if status_code == 200 :
         prediction_status = 'FAILURE'
     
 elif status_code != 200:
-    #test_status = 'FAILURE'
     score_status = 'FAILURE'
 
 output_4 =  output_4.format(image_name_2=image_name_2,                           
@@ -426,36 +402,26 @@ output_4 =  output_4.format(image_name_2=image_name_2,
 
 print(output_4)
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
         print("LOGs saved on",  log_fname)
-        #file.write(test_title)
         file.write(output_4)
 else : 
      print("An error occurred when tring to save LOGs!")
-# requête #4 END =========================================================================================
+#Request #4 END =========================================================================================
 
-# requête #5 START =========================================================================================
-
+#Request #5 START =======================================================================================
 #designation_text_3 = "Black Rock Shooter Tv Animation Figurine Figma Dead Master 16 Cm"
 #description_text_3 =  None
-
-
 #image_name_3 = './image_samples/image_955830017_product_220445101.jpg'
-
 #real_class_5 = 1140
 #real_label_5 = 'figurines and Toy Pop'
 
 
-
 designation_text_3 = "3-in-1 Manuel Tube Cintreuse application dans le remplacement des lignes de frein Tube de travail tools 1385"
 description_text_3 =  "3-in-1 manuel Tube Tube Bender application dans le remplacement des lignes de frein Tube de travail électrique automobile Caractéristiques: cintreuse de tube pour le tube de 90 degrés ou à 180 degrés de flexion conception à trois fentes fonctionne pour 6/8/10 mm tubes et tuyaux poignée ergonomique  correspond à vos mains confortablement et facile à contrôler Convient aux tubes de cuivre des tuyaux en aluminium et les tubes en acier à paroi mince une large application dans le remplacement des lignes de frein travail du tube des champs électriques de l&#39;automobile etc. spécifications: Matériel: alliage d&#39;aluminium angle de flexion maximale: 180 degres de diamètre Bend: 6/8/10 mm poids: 03350 kg taille du produit: 2030 x 590 x 400 cm / 799 x 232 x 157 pouces poids du paquet: 0.3500 kg taille du paquet: 2100 x 700 x 500 cm / 827 x 276 x 197 pouces Contenu de l&#39;emballage: 1 x Tube Bender"
-
-
 image_name_3 = './image_samples/image_1314429107_product_4202302612.jpg'
-
 real_class_5 = 2585
 real_label_5 = 'gardening and DIY'
 
@@ -502,10 +468,10 @@ actual Precision  = {precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_5.status_code
 
-# score de la requête
+# Request response
 results_5 = response_5.json()
 #results_5
 
@@ -513,7 +479,7 @@ predicted_class = results_5["predicted_class"]
 predicted_label = results_5["predicted_label"]
 precision = results_5["precision"]
 
-# affichage des résultats code response
+# Code response result
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_5:    
@@ -522,7 +488,6 @@ if status_code == 200 :
         prediction_status = 'FAILURE'
     
 elif status_code != 200:
-    #test_status = 'FAILURE'
     score_status = 'FAILURE'
 
 output_5 =  output_5.format(designation_text_3=designation_text_3,                           
@@ -539,26 +504,21 @@ output_5 =  output_5.format(designation_text_3=designation_text_3,
 
 print(output_5)
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
         print("LOGs saved on",  log_fname)
-        #file.write(test_title)
         file.write(output_5)
 else : 
      print("An error occurred when tring to save LOGs!")
-# requête #5 END =========================================================================================
-# requête #6 START =========================================================================================
+#Request #5 END ===========================================================================================
 
+#Request #6 START =========================================================================================
 designation_text_4 = "Black Rock Shooter Tv Animation Figurine Figma Dead Master 16 Cm"
 description_text_4 =  None
-
 image_name_4 = './image_samples/image_955830017_product_220445101.jpg'
-
 real_class_6 = 1140
 real_label_6 = 'figurines and Toy Pop'
-
 
 #designation_text_4 = "3-in-1 Manuel Tube Cintreuse application dans le remplacement des lignes de frein Tube de travail tools 1385"
 #description_text_4 =  "3-in-1 manuel Tube Tube Bender application dans le remplacement des lignes de frein Tube de travail électrique automobile Caractéristiques: cintreuse de tube pour le tube de 90 degrés ou à 180 degrés de flexion conception à trois fentes fonctionne pour 6/8/10 mm tubes et tuyaux poignée ergonomique  correspond à vos mains confortablement et facile à contrôler Convient aux tubes de cuivre des tuyaux en aluminium et les tubes en acier à paroi mince une large application dans le remplacement des lignes de frein travail du tube des champs électriques de l&#39;automobile etc. spécifications: Matériel: alliage d&#39;aluminium angle de flexion maximale: 180 degres de diamètre Bend: 6/8/10 mm poids: 03350 kg taille du produit: 2030 x 590 x 400 cm / 799 x 232 x 157 pouces poids du paquet: 0.3500 kg taille du paquet: 2100 x 700 x 500 cm / 827 x 276 x 197 pouces Contenu de l&#39;emballage: 1 x Tube Bender"
@@ -609,10 +569,10 @@ actual Precision={precision}
 
 '''
 
-# statut de la requête
+# Request status
 status_code = response_6.status_code
 
-# score de la requête
+# Request response
 results_6 = response_6.json()
 #results_6
 
@@ -620,7 +580,7 @@ predicted_class = results_6["predicted_class"]
 predicted_label = results_6["predicted_label"]
 precision = results_6["precision"]
 
-# affichage des résultats code response
+# Code response result
 if status_code == 200 :
     test_status = 'SUCCESS'
     if predicted_class == real_class_6:    
@@ -629,7 +589,6 @@ if status_code == 200 :
         prediction_status = 'FAILURE'
     
 elif status_code != 200:
-    #test_status = 'FAILURE'
     score_status = 'FAILURE'
 
 output_6 =  output_6.format(designation_text_4=designation_text_4,                           
@@ -654,16 +613,14 @@ test_end ='''
 **********************************************************************************************
 '''
 
-# impression dans un fichier
-
+# Print logs in a file
 if os.environ.get('LOG') == '1':
     with open(log_fname, 'a') as file:
         print("LOGs saved on",  log_fname)
-        #file.write(test_title)
         file.write(output_6)
         file.write(test_end)
 else : 
      print("An error occurred when tring to save LOGs!")
 print(test_end)
-# requête #6 END =========================================================================================
+#Request #6 END =========================================================================================
 
